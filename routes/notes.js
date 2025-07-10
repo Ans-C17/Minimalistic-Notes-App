4357f8e487790cab89f3e1f73761afb473e327ae
Ans Christy <jovanacarmel@gmail.com>
2025-07-10 12:22:04 +0530

    Server responds to edit request

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
-const notesPath = path.join(__dirname, '../data.js');
+const notesPath = path.join(__dirname, '../data.json');
 
 function saveNotes(newNote){
     const notes = fs.readFileSync(notesPath, 'utf-8');
@@ -28,6 +28,18 @@ router.post('/', (req, res) => {
 router.get('/', (req, res) => {
     const notes = readNotes();
     res.status(200).json(notes);
+})
+
+router.patch('/:id', (req, res) => {
+    const id = +req.params.id;
+    const {title, noteBody} = req.body;
+    const notes = readNotes();
+    const index = notes.findIndex(note => note.id === id);
+    if (index == -1) return res.status(404).send("Note not found");
+    notes[index].title = title;
+    notes[index].noteBody = noteBody;
+    fs.writeFileSync(notesPath, JSON.stringify(notes, '', 2));
+    res.status(200).send("Note updated");
 })
 
 router.delete('/:id', (req, res) => {
